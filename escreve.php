195ded80f56cee2e20ba537f784d57df42549e4c
Makerjunior <92755114+Makerjunior@users.noreply.github.com>
2023-06-22 19:44:26 +0000

    Initial commit

---
+++ escreve.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Formulario</title>
+
+</head>
+
+<body>
+<?php
+
+$nome=$_POST['f_nome'];
+$msg=$_POST['f_msg'];
+$conteudo="\r\n"."<h3 style='color:#00f'>".$nome."</h3>"."\r\n".$msg."\r\n";
+$arquivo=fopen("visitas.txt","a");
+fwrite($arquivo,$conteudo);
+  //echo "Mensagem gravada :$conteudo";
+ echo "Mensagem enviada <br/>";
+fclose($arquivo);
+?>
+<a href="index.html" target="_self">Voltar</a>
+
+</body>
+
+</html>
---
+++ ler.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Formulario</title>
+
+</head>
+
+<body>
+<?php
+
+/*
+  // Forma simples 
+$arquivo=fopen("visitas.txt","r");
+//echo $linhas = count(file('visitas.txt'));
+ while(!feof($arquivo)){ // Enquando  nao estiver no final do arquixo
+
+   echo fgets($arquivo,50); // retrorna a linha 0 do arquivo ate 50 caracteres
+   echo "<br/>".fgets($arquivo,500);// retrorna a linha 1 do arquivo ate 500 caracteres 
+ echo fgets($arquivo,500);// retrorna a linha 1 do arquivo ate 500 caracteres 
+
+   echo "</br> <br/>";   
+                      
+ }
+
+fclose($arquivo); 
+*/
+
+
+// Forma avançada 
+echo "<h2>Mensagens</h2>";
+$names=file('visitas.txt');
+// To check the number of lines
+
+foreach($names as $name)
+{
+
+if($name == "\r\n"){
+echo "<hr>";
+} 
+echo $name.'<br>';
+}
+
+
+?>
+<hr>
+<a href="index.html" target="_self">Voltar</a>
+  
+</body>
+
+</html>
